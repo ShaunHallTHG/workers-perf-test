1ef1e5ece958903b2566c6bb9d2089693314ee17
Shaun Hall <shaun.hall@thehutgroup.com>
2023-10-24 15:25:18 +0100

    first add

---
+++ README.md
@@ -0,0 +1,11 @@
+# Requirements
+* apachebench
+* bash
+* gnuplot
+* sed
+
+
+Run `perf-tests.sh`
+
+
+Credit to this repo for original versions of `ab-*.sh` scripts [apachebench-graphs](https://github.com/juanluisbaptiste/apachebench-graphs/tree/master)
---
+++ perf-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+declare -A sites
+sites[vanillaJS]="a4f1e09c"
+sites[astroStarter]="67329b3f"
+sites[altitudeMTCWithMiddleware]="a7cbe441"
+sites[altitudeMTCNoMiddleware]="3c34dea7"
+rm -rf results
+
+for key in "${!sites[@]}"
+do 
+	echo "Testing $key"
+	./ab-graph.sh -l "$key" -n 100 -c 1 -E  "-H 'x-altitude-instance: altitude.www.allsole.com' -H \"X-Forwarded-Host: ${sites[$key]}.thgaltitude.com\" -H 'X-Forwarded-Proto: https' " -u https://${sites[$key]}.cf-thgaltitude.com/hello  
+done
+./ab-graph_merge.sh $PWD/results/*/*
