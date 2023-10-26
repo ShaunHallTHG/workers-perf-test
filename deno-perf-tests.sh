54e3d70474de52a18fd6e84d61c72a4f2dbaf13e
Shaun Hall <shaun.hall@thehutgroup.com>
2023-10-26 13:45:25 +0100

    only test CF without EP

--- deno-perf-tests.sh
+++ deno-perf-tests.sh
@@ -3,9 +3,9 @@
 declare -A sites
 #sites[vanillaJS]="a4f1e09c"
 #sites[vanillaJSWithAstroBlob]="ac1d3adc"
-sites[cloudflareWithEPAstroStarterBlog]="67329b3f.cf-thgaltitude.com/blog"
+#sites[cloudflareWithEPAstroStarterBlog]="67329b3f.cf-thgaltitude.com/blog"
 sites[cloudflareAstroStarterBlog]="dc256aeebfd5710a-ssr.shaun-hall.workers.dev/blog"
-sites[cloudflareWithEPAstroStarterBlogHello]="67329b3f.cf-thgaltitude.com/hello"
+#sites[cloudflareWithEPAstroStarterBlogHello]="67329b3f.cf-thgaltitude.com/hello"
 sites[cloudflareAstroStarterBlogHello]="dc256aeebfd5710a-ssr.shaun-hall.workers.dev/hello"
 #sites[altitudeMTCWithMiddleware]="a7cbe441"
 #sites[altitudeMTCNoMiddleware]="3c34dea7"
@@ -15,7 +15,7 @@ rm -rf results
 
 for key in "${!sites[@]}"
 do 
-	echo "Testing $key"
+	echo "Testing $key (https://${sites[$key]})"
 	#./ab-graph.sh -l "$key" -n 100 -c 1 -E  "-H 'x-altitude-instance: altitude.www.allsole.com' -H \"X-Forwarded-Host: ${sites[$key]}.thgaltitude.com\" -H 'X-Forwarded-Proto: https' " -u https://${sites[$key]}.cf-thgaltitude.com/hello  
 	./ab-graph.sh -l "$key" -n 100 -c 1 -u https://${sites[$key]}  
 done
